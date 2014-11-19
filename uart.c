837776be93187d657c8d3336324c73e5897202ff
David Yao <excsniper@gmail.com>
2014-11-19 02:56:50 -0500

    Updated UART code

--- uart.c
+++ uart.c
@@ -4,110 +4,143 @@
 #include "CU_TM4C123.h"
 #include "uart.h"
 
-void __attribute__((weak)) UART0_Rx()
+void uart_int_off(UART0_Type *uart)
 {
-    uart_sendstr("ERROR: Bytes received but UART0_Rx() not implemented!\n\r");
-    while(UART0->MIS & (UART_MIS_RXMIS | UART_MIS_RTMIS))
-        UART0->DR;  // flush the FIFO
+    uart->IM &= ~(UART_IM_RXIM | UART_IM_RTIM);
 }
 
-void UART0_Handler()
+void uart_int_on(UART0_Type *uart)
 {
-    if (UART0->MIS & (UART_MIS_RXMIS | UART_MIS_RTMIS))
-    {
-        UART0_Rx();
-        /*  Interrupt cleared by emptying FIFO  */
-    }
-    else
-    {
-        /*  Other errors  */
-    }
+    uart->IM |= UART_IM_RXIM | UART_IM_RTIM;
 }
 
 /*
-    Configure UART0 to do 9600 baud, 8-N-1
-    UART0 uses PA0 & PA1
+    Configure UART to baud, 8-N-1, interrupt on Rx
+    Returns -1 if no init code is available  otherwise 0 for success
+    Don't forget to implement the RX ISRs
 */
-void uart_init(uint16_t baud)
+int uart_init(UART0_Type *uart, uint32_t baud)
 {
-    // enable clock to Port A
-    SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R0;
-    // enable clock to UART0
-    SYSCTL->RCGCUART |= SYSCTL_RCGCUART_R0;
-	
-	// dummy read
-    SYSCTL->RCGCGPIO;
-    
-    // select the alternate function for PA0 & PA1
-    GPIOA->AFSEL |= (1<<0) | (1<<1);
-	// configure the pins to be U0RX & U0TX
-    GPIOA->PCTL |= GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX;
-    // enable the digital pad for PA0 & PA1
-    GPIOA->DEN |= (1<<0) | (1<<1);
-    
+    if (uart == UART0)
+    {
+        // UART0_RX = PA0
+        // UART0_TX = PA1
+        
+        // enable clock to Port A
+        SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R0;
+        // enable clock to UART0
+        SYSCTL->RCGCUART |= SYSCTL_RCGCUART_R0;
+        
+        // select the alternate function for PA0 & PA1
+        GPIOA->AFSEL |= (1<<0) | (1<<1);
+        // configure the pins to be U0RX & U0TX
+        GPIOA->PCTL |= GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX;
+        // enable the digital pad for PA0 & PA1
+        GPIOA->DEN |= (1<<0) | (1<<1);
+        
+        // enable the interrupt from the NVIC
+        NVIC_EnableIRQ(UART0_IRQn);
+    }
+    else if (uart == UART5)
+    {
+        // UART5_RX = PE4
+        // UART5_TX = PE5 
+        
+        // enable clock to Port E
+        SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R4;
+        // enable clock to UART5
+        SYSCTL->RCGCUART |= SYSCTL_RCGCUART_R5;
+        
+        // select the alternate function for PE4 & PE5
+        GPIOE->AFSEL |= (1<<4) | (1<<5);
+        // configure the pins to be U5RX & U5TX
+        GPIOE->PCTL |= GPIO_PCTL_PE4_U5RX | GPIO_PCTL_PE5_U5TX;
+        // enable the digital pad for PE4 & PE5
+        GPIOE->DEN |= (1<<4) | (1<<5);
+        
+        // enable the interrupt from the NVIC
+        NVIC_EnableIRQ(UART5_IRQn);
+    }
+    else
+    {
+        // No init for specifed module
+        return -1;
+    }
+        
     // calculate the int part of the baud rate divider
-    UART0->IBRD = SystemCoreClock / (16 * baud);
+    uart->IBRD = SystemCoreClock / 16 / baud;
     // calculate the fractional part of the baud rate divider
-    UART0->FBRD = (((SystemCoreClock / (16 * baud) << 7) + 1) >> 1) & UART_FBRD_DIVFRAC_M;
+    uart->FBRD = (((((SystemCoreClock << 3) / baud)) + 1) >> 1) & UART_FBRD_DIVFRAC_M;
     
     // configure the word length to be 8 bits
-    UART0->LCRH |= UART_LCRH_WLEN_8 | UART_LCRH_FEN;
+    uart->LCRH |= UART_LCRH_WLEN_8 | UART_LCRH_FEN;
     // set the RX FIFO Trigger level
-    UART0->IFLS |= FIFO_TRIG_LVL;
-    // enable RX interrupts
-    UART0->IM |= UART_IM_RXIM | UART_IM_RTIM;
+    uart->IFLS |= FIFO_TRIG_LVL;
+    // DO NOT DO THIS BY DEFAULT: enable RX interrupts
+    // uart_int_on(uart);
     // enable TX and RX
-    UART0->CTL |= UART_CTL_UARTEN;
+    uart->CTL |= UART_CTL_UARTEN;
     
-    // enable the interrupt from the NVIC
-    NVIC_EnableIRQ(UART0_IRQn);
+    return 0;
 }
 
-uint8_t uart_sendbyte(uint8_t b)
+uint8_t uart_sendbyte(UART0_Type *uart, uint8_t b)
 {
-    UART0->DR = b;
-    while(UART0->FR & UART_FR_TXFF);    // busy wait until the 16 byte TX FIFO is no longer full
+    uart->DR = b;
+    while(uart->FR & UART_FR_TXFF);    // busy wait until the 16 byte TX FIFO is no longer full
     return b;
 }
 
-void uart_sendbytes(void *buf, uint16_t len)
+void uart_sendbytes(UART0_Type *uart, void *buf, uint16_t len)
 {
-    uint8_t *bufptr = (uint8_t*) buf;
     while(len--)
     {
-        uart_sendbyte(*(bufptr++));
-    }
-}
-
-void uart_sendstr(void *buf)
-{
-    while(*(char*)buf)
-    {
-        uart_sendbyte(*(char*)buf);
+        uart_sendbyte(uart, *(char *)buf);
         buf = (char*)buf + 1;
     }
 }
 
-int uart_getbyte()
+void uart_sendstr(UART0_Type *uart, void *buf)
 {
-    if (UART0->FR & UART_FR_RXFE)
+    while(*(char*)buf)
+    {
+        uart_sendbyte(uart, *(char*)buf);
+        buf = (char*)buf + 1;
+    }
+}
+
+int uart_getbyte(UART0_Type *uart)
+{
+    if (uart->FR & UART_FR_RXFE)
     {
         return EOF;
     }
     else
     {
-        return UART0->DR;
+        return uart->DR;
     }
 }
 
-uint16_t uart_getbytes(void *buf, uint16_t len)
+uint16_t uart_getbytes(UART0_Type *uart, void *buf, uint16_t len)
 {
     int c;
     uint8_t *bufptr = (uint8_t*) buf;
-    while(len-- && ((c = uart_getbyte()) != EOF))
+    while(len-- && ((c = uart_getbyte(uart)) != EOF))
     {
         *bufptr = c;
         bufptr++;
     }
     return (uint32_t)bufptr-(uint32_t)buf;
 }
+
+#ifdef __RINGBUF_H
+int uart_getbytes_ringbuf(UART0_Type *uart, RING_BUF_t *buf)
+{
+    int c, rc;
+#pragma diag_suppress   1293
+    while( ((c = uart_getbyte(uart)) != EOF) && !(rc = (ringbuf_put(buf, c))) )
+        ;
+#pragma diag_warning    1293
+    return rc;
+}
+#endif
